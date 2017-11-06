3af9709fb1221ae5ff7cbb4397f36a0fc05e836e
Franco Catena <francocatena@gmail.com>
2017-11-05 22:37:46 -0300

    Get rid of az plugin, hope I don't need it anymore

--- .oh-my-zsh/custom/plugins/az/az.plugin.zsh
+++
@@ -1,7 +0,0 @@
-AZ_ROOT_DIR=$HOME/.azure-cli
-AZ_BIN_DIR=$AZ_ROOT_DIR/bin
-
-if [[ -d $AZ_ROOT_DIR && -d $AZ_BIN_DIR ]]; then
-  export PATH=$PATH:$AZ_BIN_DIR
-  source "$AZ_ROOT_DIR/az.completion"
-fi
